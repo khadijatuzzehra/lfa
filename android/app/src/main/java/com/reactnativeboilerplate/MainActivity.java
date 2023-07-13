69367a71311202ee95a9c235aaa2ab03c9ad9a9a
Khadija <zaidikhadija462@gmail.com>
2023-07-13 16:41:52 +0500

    React Native Template

--- android/app/src/main/java/com/reactnativeboilerplate/MainActivity.java
+++ android/app/src/main/java/com/reactnativeboilerplate/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeBoilerPlate";
+    return "Look for Africa";
   }
 
 
