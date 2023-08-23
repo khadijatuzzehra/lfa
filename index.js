e4ba5e56e0c1b53927a9682d83ba83672b53cb31
Khadija <zaidikhadija462@gmail.com>
2023-08-23 11:46:13 +0500

    Ios changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 /**
  * @format
  */
@@ -6,37 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-// import './BgFetchService'
-import messaging from '@react-native-firebase/messaging';
-import notifee, {
-  EventType,
-  AndroidImportance,
-  TriggerType,
-  Trigger,
-} from '@notifee/react-native';
-
-const onMessageReceived = async message => {
-  const notification = message.notification;
-  notification.android = {...notification.android, channelId: 'default'};
-  await notifee.displayNotification(notification);
-};
-
-messaging().setBackgroundMessageHandler(onMessageReceived);
-messaging().onMessage(onMessageReceived);
-
-notifee.onBackgroundEvent(async ({type, detail}) => {
-  const {notification, pressAction} = detail;
-  if (type === EventType.PRESS) {
-    await notifee.cancelNotification(notification.id);
-  }
-});
-
-notifee.onForegroundEvent(async ({type, detail}) => {
-  const {notification, pressAction} = detail;
-
-  if (type === EventType.PRESS) {
-    await notifee.cancelNotification(notification.id);
-  }
-});
 
 AppRegistry.registerComponent(appName, () => App);
